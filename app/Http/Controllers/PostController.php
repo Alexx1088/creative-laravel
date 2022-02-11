0d7f95491b6d4e2ce6c88bd892d0cdd37cfc15d5
Alexx <alexx1088@gmail.com>
2022-02-11 17:51:35 +0600

    11.02

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,9 +8,100 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
    public function index() {
+/*
+       $posts = Post::all();
 
-       $post = Post::find(1);
-       dd($post->title);
+       foreach ($posts as $post) {
+
+           dump($post->image);
+       }*/
+
+      // dd($posts);
+
+      /* $post = Post::where('is_published', 0)->first();
+
+       dump($post->title);*/
+
+       $posts = Post::where('is_published', 0)->get();
+
+       foreach ($posts as $post) {
+
+      dump($post->title);
+      // echo    gettype($posts);
+
+       }
+
+    /*   foreach ($post as $post) {
+          dump($post->content);
+       }*/
+
+    //   dump($post->title);
+
+
+     //  dd('end');
           }
+
+    public function create() {
+
+       $postsArr = [
+
+           ['title' => 'title of post from phpstorm',
+           'content' => 'some interesting content',
+           'image' => 'imageblabla.jpg',
+           'likes' => '25',
+           'is_published' => '1',
+       ],
+
+           ['title' => 'another title of post from phpstorm',
+               'content' => 'another some interesting content',
+               'image' => 'another imageblabla.jpg',
+               'likes' => '55',
+               'is_published' => '1',
+           ]
+];
+       foreach ($postsArr as $item) {
+           Post::create($item);
+       }
+
+      /* Post::create([
+           'title' => 'another title of post from phpstorm',
+           'content' => 'another some interesting content',
+           'image' => 'another imageblabla.jpg',
+           'likes' => '55',
+           'is_published' => '1',
+                  ]);*/
+       dd('created');
+    }
+
+    public function update() {
+
+       $post = Post::find(11);
+
+    // dump($post->content);
+
+        $post->update([
+            'title' => 'updated',
+            'content' => 'updated',
+            'image' => 'updated',
+            'likes' => '1000',
+            'is_published' => '0',
+        ]);
+
+        dd('updated');
+
+    }
+
+    public function delete() {
+
+        $post = Post::withTrashed()->find(2);
+
+       //  dump($post->content);
+
+        $post->restore();
+
+        dd('deleted');
+
+    }
+
 }
 
